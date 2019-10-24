79a992c5281064bac886a978f0b31147df45c34e
Christopher Crawford <christopher@digitonic.co.uk>
2019-10-24 16:05:35 +0100

    Add tests and remove composer lock file

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Digitonic\PassonaClient\Tests;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use Orchestra\Testbench\TestCase;
+
+class BaseTestCase extends TestCase
+{
+    /**
+     * @var \Digitonic\PassonaClient\Client
+     */
+    protected $client;
+
+    /**
+     * @var Response|null
+     */
+    protected $response = null;
+
+    /**
+     *
+     * @return void
+     */
+    protected function setConfig(): void
+    {
+        $this->app['config']->set('passona-sdk.passona_api_key', 'KxDMt9GNVgu6fJUOG0UjH3d4kjZPTxFiXd5RnPhUD8Qz1Q2esNVIFfqmrqRD');
+        $this->app['config']->set('passona-sdk.passona_team_uuid', '4dc3d890-f63c-11e2-bac2-0a58646002d8');
+        $this->app['config']->set('passona-sdk.passona_base_uri', 'https://passona.co.uk/api/2.0/');
+    }
+
+    /**
+     * @param Response $response
+     * @return \Digitonic\PassonaClient\Client\
+     */
+    protected function setupMockedClient(Response $response): \Digitonic\PassonaClient\Client
+    {
+        $mock = new MockHandler([$response]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        return new \Digitonic\PassonaClient\Client($client);
+    }
+}
