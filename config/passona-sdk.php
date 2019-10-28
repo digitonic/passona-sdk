aac06f1b487efd39da6c872e678da9ae66a32570
Christopher Crawford <christopher@digitonic.co.uk>
2019-10-28 14:26:37 +0000

    Update config comments

--- config/passona-sdk.php
+++ config/passona-sdk.php
@@ -1,6 +1,34 @@
 <?php
 return [
+    /*
+     |--------------------------------------------------------------------------
+     | Passona API Key
+     |--------------------------------------------------------------------------
+     |
+     | Here you may specify your Passona API Key. This key will be
+     | required for every request you make to Passona via the SDK.
+     |
+     */
     'passona_api_key' => env('PASSONA_API_KEY'),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Passona Team UUID
+     |--------------------------------------------------------------------------
+     |
+     | Here you may specify your Passona Team UUID.
+     |
+     */
     'passona_team_uuid' => env('PASSONA_TEAM_UUID'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Passona Base URI
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the Passona Base URL for API requests.
+    | This URL is used to configure the client for every request.
+    |
+    */
     'passona_base_uri' => env('PASSONA_BASE_URI'),
 ];
